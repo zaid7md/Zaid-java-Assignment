8bf234663037d761ffe6302d9c422f3dc3825aa4
zaid7md <128227564+zaid7md@users.noreply.github.com>
2023-10-20 00:56:00 +0530

    derived.java

--- derived.java
+++ derived.java
@@ -2,6 +2,7 @@ package market ;
 import java.util.Scanner; 
 interface MarketPlace//interface 
 {
+    //variables and arrays taken in interface so that they are static and final by defualt and cannot be further changed in the program 
     int maxMoney = 10000 ; //public + final + static 
 
     String roleplay[] = {"GTA","READ DEAD REMPTION","ELDEN RING","SEKIRO SHADOWS DIES TWICE","LAST OF US"}; 
@@ -21,7 +22,7 @@ interface MarketPlace//interface
     int sportsPrice[] = {2500,1000,1200,600,200};
 
 }
-interface budget //interface 
+interface budget //interface listing the methods that must be overriden in the class which implements it  
 {
     void buyRoleplay() ; 
     void buyracing();
@@ -31,16 +32,16 @@ interface budget //interface
     void display();
 }
 
-public class derived implements budget , MarketPlace//multiple ineheritence 
+public class derived implements budget , MarketPlace //multiple ineheritence where dervied class implements two interfaces at once 
 {
-    public static int budget = MarketPlace.maxMoney;
-    public static int total = 0 ; 
-    public static int RemainingBudget ; 
-    static String selectedgames[] = new String[100]; 
-    static int count = 0 ; 
+    public static int budget = MarketPlace.maxMoney; //static
+    public static int total = 0 ; // made static to calculate the total money spent on games
+    public static int RemainingBudget ; // static to calculate the reaming money left 
+    static String selectedgames[] = new String[100]; //cart whose maxx capacity is 100 and is made static 
+    static int count = 0 ; //static to maintain the count of of items in cart 
     Scanner sc = new Scanner(System.in) ; 
 
-    public void buyRoleplay()
+    public void buyRoleplay() // buying roleplay games and updating count , total money spent , remaining budget 
     {
         
          for(int i = 0 ; i < 5 ; i++)
@@ -50,10 +51,10 @@ public class derived implements budget , MarketPlace//multiple ineheritence
         int choice = sc.nextInt() ; 
         if(choice >=1 && choice <= 5 && (total + MarketPlace.roleplayPrice[choice -1]) <= 10000)
         {
-            total += MarketPlace.roleplayPrice[choice-1];          
-            RemainingBudget = budget - total ;                                                                                                      
-            selectedgames[count] = MarketPlace.roleplay[choice-1]; 
-            count++ ; 
+            total += MarketPlace.roleplayPrice[choice-1];  //updating money spent     
+            RemainingBudget = budget - total ; //updating money spent                                                                    
+            selectedgames[count] = MarketPlace.roleplay[choice-1]; //updating money spent 
+            count++ ; //updating the count value of items in the cart 
         }
         else if(choice > 5 || choice < 1 )
         {
@@ -73,12 +74,12 @@ public class derived implements budget , MarketPlace//multiple ineheritence
             System.out.printf("%d) %s - %d\t",i+1,MarketPlace.racingGame[i],MarketPlace.racingGamePrice[i]);
         }
         int choice = sc.nextInt() ; 
-        if(choice >=1 && choice <= 5 && total<=10000)
+        if(choice >=1 && choice <= 5 && (total + MarketPlace.racingGamePrice[choice -1]) <= 10000)
         {
-            total += MarketPlace.racingGamePrice[choice-1];          
-            RemainingBudget = budget - total ;                                                                                                      
-            selectedgames[count] = MarketPlace.racingGame[choice-1]; 
-            count++ ; 
+            total += MarketPlace.racingGamePrice[choice-1];     //updating the total money spent     
+            RemainingBudget = budget - total ;  //updating remaining money                                                                                                     
+            selectedgames[count] = MarketPlace.racingGame[choice-1]; //updating the cart 
+            count++ ; //updating the count value of items in the cart
         }
         else if(choice > 5 || choice < 1 )
         {
@@ -97,12 +98,12 @@ public class derived implements budget , MarketPlace//multiple ineheritence
             System.out.printf("%d) %s - %d\t",i+1,MarketPlace.horror[i],MarketPlace.horrorPrice[i]);
         }
         int choice = sc.nextInt() ; 
-        if(choice >=1 && choice <= 5 && total<=10000)
+        if(choice >=1 && choice <= 5 && (total + MarketPlace.horrorPrice[choice -1]) <= 10000)
         {
-            total += MarketPlace.horrorPrice[choice-1];          
-            RemainingBudget = budget - total ;                                                                                                      
-            selectedgames[count] = MarketPlace.horror[choice-1]; 
-            count++ ; 
+            total += MarketPlace.horrorPrice[choice-1];  //updating the total money spent      
+            RemainingBudget = budget - total ;    //updating remaining money                                                                                                   
+            selectedgames[count] = MarketPlace.horror[choice-1]; //updating the cart 
+            count++ ; //updating the count value of items in the cart
         }
         else if(choice > 5 || choice < 1 )
         {
@@ -120,12 +121,12 @@ public class derived implements budget , MarketPlace//multiple ineheritence
             System.out.printf("%d) %s - %d\t",i+1,MarketPlace.fps[i],MarketPlace.fpsPrice[i]);
         }
         int choice = sc.nextInt() ; 
-        if(choice >=1 && choice <= 5 && total<=10000)
+        if(choice >=1 && choice <= 5 && (total + MarketPlace.fpsPrice[choice -1]) <= 10000)
         {
-            total += MarketPlace.fpsPrice[choice-1];          
-            RemainingBudget = budget - total ;                                                                                                      
-            selectedgames[count] = MarketPlace.fps[choice-1]; 
-            count++ ; 
+            total += MarketPlace.fpsPrice[choice-1];     //updating the total money spent       
+            RemainingBudget = budget - total ;  //updating the remaining money                                                                                                    
+            selectedgames[count] = MarketPlace.fps[choice-1]; //updating the cart
+            count++ ; //updating the count value of items in the cart
         }
         else if(choice > 5 || choice < 1 )
         {
@@ -143,12 +144,12 @@ public class derived implements budget , MarketPlace//multiple ineheritence
             System.out.printf("%d) %s - %d\t",i+1,MarketPlace.sports[i],MarketPlace.sportsPrice[i]);
         }
         int choice = sc.nextInt() ; 
-        if(choice >=1 && choice <= 5 && total<=10000)
+        if(choice >=1 && choice <= 5 && (total + MarketPlace.sportsPrice[choice -1]) <= 10000)
         {
-            total += MarketPlace.sportsPrice[choice-1];          
-            RemainingBudget = budget - total ;                                                                                                      
-            selectedgames[count] = MarketPlace.sports[choice-1]; 
-            count++ ; 
+            total += MarketPlace.sportsPrice[choice-1];   //updating the total money spent        
+            RemainingBudget = budget - total ;   //updating the remaining money                                                                                            
+            selectedgames[count] = MarketPlace.sports[choice-1]; //updating the cart
+            count++ ; //updating the count value of items in the cart
         }
         else if(choice > 5 || choice < 1 )
         {
@@ -176,4 +177,7 @@ public class derived implements budget , MarketPlace//multiple ineheritence
             System.out.println("Empty cart!");
         }
     }
+    // all methods of the interfaces overridden 
+    //interface MarketPlace used for declaring final static variables and arrays 
+    // interface budget used for mentioning a list of methods to be overriden in the class 
 }
